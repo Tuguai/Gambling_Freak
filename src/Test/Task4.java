842b57612a0646513e57b3d08277f72579da6e37
Yuelin-bit <yuelinapp@163.com>
2020-01-27 13:43:02 -0500

    tasks frame

--- src/Test/Task4.java
+++ src/Test/Task4.java
@@ -1,6 +1,18 @@
 package Test;
+import Application.FightTheLandlordApplication;
 import Controller.Controller;
 public class Task4 {
-	//Task3-3:add here
 
+	public static void main(String args[]) {
+	Controller.initailizeGame();
+		boolean pass = true;
+		if(FightTheLandlordApplication.getFL()==null) {
+			pass = false;
+			System.out.println("Fail--1");
+		}
+		
+		//Task3-3:add here
+		
+		System.out.println(pass?"Congratulations! You passed!" : "No! there is still some bug!");
+	}
 }
