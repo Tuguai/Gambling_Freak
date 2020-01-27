66922c5531074e8ce06563cd683a6671a1286d4a
Yuelin <yuelin.liu@mail.mcgill.ca>
2020-01-27 00:59:54 -0500

    task1

--- src/Test/Task1.java
+++ src/Test/Task1.java
@@ -1,12 +1,17 @@
 package Test;
+import Application.FightTheLandlordApplication;
 import Controller.Controller;
 import Model.Board;
 public class Task1 {
 	public static void main(String args[]) {
 		//Task3-2:add here
 		Controller.initailizeGame();
-		
-		
+		boolean pass = true;
+		if(FightTheLandlordApplication.getFL()==null) {
+			pass = false;
+			System.out.println("Fail--1");
+		}
+			
 	}
 	
 
